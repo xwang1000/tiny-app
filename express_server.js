78a73a0b6113ded3180b899928515c3a134396b9
xw <23066125+xwang1000@users.noreply.github.com>
2019-02-12 14:37:13 -0800

    Add new url submission form

--- express_server.js
+++ express_server.js
@@ -2,6 +2,9 @@ var express = require("express");
 var app = express();
 var PORT = 8080; // default port 8080
 
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
+
 app.set('view engine', 'ejs');
 
 var urlDatabase = {
@@ -31,8 +34,22 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+});
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
 app.get("/urls/:shortURL", (req, res) => {
   const matchLongURL = urlDatabase[req.params.shortURL]
   let templateVars = { shortURL: req.params.shortURL, longURL: matchLongURL };
   res.render("urls_show", templateVars);
 });
+
+function generateRandomString() {
+  return Math.random().toString(36).slice(6)
+}
+console.log(generateRandomString())
