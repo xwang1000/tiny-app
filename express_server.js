d88567499a13e14130e0af31becc379278f5f475
xw <23066125+xwang1000@users.noreply.github.com>
2019-02-12 13:31:19 -0800

    Add url route and template view

--- express_server.js
+++ express_server.js
@@ -22,5 +22,11 @@ app.get("/urls.json", (req, res) => {
 });
 
 app.get("/hello", (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n");
+  let templateVars = { greeting: 'Hello World!' };
+  res.render("hello_world", templateVars);
+});
+
+app.get("/urls", (req, res) => {
+  let templateVars = { urls: urlDatabase};
+  res.render("urls_index", templateVars);
 });
